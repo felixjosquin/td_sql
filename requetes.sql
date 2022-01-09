ae46489d94bb627f263c980a59fb9d464d268be5
Enzo Damion <enzo.damion@depinfonancy.net>
2022-01-09 22:36:45 +0100

    commentaires fr

--- requetes.sql
+++ requetes.sql
@@ -1,38 +1,38 @@
 -- "------------------ QUESTION 3 ------------------"
-SELECT Classes.Enseignant, GROUP_CONCAT(Eleves.nom) AS Liste_eleves -- create a string of students' name separated by a ","
+SELECT Classes.Enseignant, GROUP_CONCAT(Eleves.nom) AS Liste_eleves -- cree une liste de noms d'eleves separes par une virgule
 FROM Classes, Eleves WHERE Classes.ClassID = Eleves.ClassID
 GROUP BY Classes.Enseignant
-ORDER BY Classes.Enseignant ASC; -- sort Enseignants' name by alphabetical order 
+ORDER BY Classes.Enseignant ASC; -- tri par noms d'enseignants dans l'ordre alphabetique
 
 
 -- "------------------ QUESTION 4 ------------------"
-SELECT Activites.Jour, Activites.Bus, COUNT(Repartition.ElevID) AS Nb_eleves -- count the number of students by Bus for each day
+SELECT Activites.Jour, Activites.Bus, COUNT(Repartition.ElevID) AS Nb_eleves -- compte le nombre d'eleves par bus pour chaque jour
 FROM Activites, Repartition WHERE Activites.ActID = Repartition.ActID
 GROUP BY Activites.Jour, Activites.Bus
-ORDER BY Activites.Jour ASC, Activites.Bus ASC; -- optional
+ORDER BY Activites.Jour ASC, Activites.Bus ASC; -- optionnel
 
 
 -- "------------------ QUESTION 5 ------------------"
 SELECT Activites.Jour, Eleves.ElevID
 FROM Activites, Repartition, Eleves WHERE Activites.ActID = Repartition.ActID AND Eleves.ElevID = Repartition.ElevID
 GROUP BY Activites.Jour, Eleves.ElevID
-HAVING COUNT(Activites.ActID) >= 2 -- select students who have more than 2 activities
-ORDER BY Activites.Jour ASC; -- optional
+HAVING COUNT(Activites.ActID) >= 2 -- garde que les eleves qui ont plus de 2 activites
+ORDER BY Activites.Jour ASC; -- optionnel
 
 
 -- "------------------ QUESTION 6 ------------------"
 SELECT Eleves.ElevID
 FROM Activites, Repartition, Eleves WHERE Activites.ActID = Repartition.ActID AND Eleves.ElevID = Repartition.ElevID
 GROUP BY Eleves.ElevID
-HAVING COUNT(DISTINCT Activites.Jour) = ( -- student must have activities the same number of distinct days as ...
-    SELECT COUNT(DISTINCT Activites.Jour) -- the number of days with activities
+HAVING COUNT(DISTINCT Activites.Jour) = ( -- les eleves doivent avoir des activites autant de jours que ...
+    SELECT COUNT(DISTINCT Activites.Jour) -- il y a de jour avec activite
     FROM Activites)
-ORDER BY Eleves.ElevID ASC; -- optional
+ORDER BY Eleves.ElevID ASC; -- optionnel
 
 
 -- "------------------ QUESTION 7 ------------------"
-SELECT COUNT(DISTINCT Eleves.ElevID) AS Nb_eleves -- count the number of students who have ...
-FROM Eleves, Repartition, Activites WHERE Eleves.ElevID=Repartition.ElevID AND Repartition.ActID=Activites.ActID AND Eleves.Ville=Activites.Lieu; -- an activity where they live
+SELECT COUNT(DISTINCT Eleves.ElevID) AS Nb_eleves -- compte le nombre d'eleves qui ...
+FROM Eleves, Repartition, Activites WHERE Eleves.ElevID=Repartition.ElevID AND Repartition.ActID=Activites.ActID AND Eleves.Ville=Activites.Lieu; -- ont une activite ou ils vivent
 
 
 -- "------------------ QUESTION 8 ------------------"
@@ -40,12 +40,12 @@ SELECT Activites.Theme,COUNT(Repartition.ElevID) AS Nb_eleves
 FROM Repartition,Activites WHERE Repartition.ActID=Activites.ActID
 GROUP BY Activites.ActID 
 HAVING COUNT(Repartition.ElevID) IN 
-    (SELECT A.Nb_eleves -- Select all number of students which have in 2 different activities in common 
-    FROM (SELECT Activites.ActID,COUNT(Repartition.ElevID) AS Nb_eleves -- Create table A containing the number of students by activity
+    (SELECT A.Nb_eleves -- Selectionne tous les nombres d'etudiants, partage par 2 activites  
+    FROM (SELECT Activites.ActID,COUNT(Repartition.ElevID) AS Nb_eleves -- Table A = nombre d'etudiants par activite 
          FROM Repartition,Activites WHERE Repartition.ActID=Activites.ActID
          GROUP BY Activites.ActID) AS A
     GROUP BY A.Nb_eleves
-    HAVING COUNT(A.ActID)>1)
+    HAVING COUNT(A.ActID)>=2)
 ORDER BY Nb_eleves DESC;
 
 
@@ -84,12 +84,12 @@ GROUP BY Activites.Theme;
 
 --  "------------------ QUESTION 13 ------------------"
 SELECT C.Theme,GROUP_CONCAT(C.Ville) AS Ville,C.Nb_eleves
-FROM (SELECT Activites.Theme,COUNT(Eleves.ElevID) AS Nb_eleves,Eleves.Ville -- all city where there is the max student in a activity 
+FROM (SELECT Activites.Theme,COUNT(Eleves.ElevID) AS Nb_eleves,Eleves.Ville -- Toutes les villes ou il y a le nombre maximal de participant dans une activite
       FROM Repartition,Activites,Eleves WHERE Activites.ActID=Repartition.ActID AND Repartition.ElevID=Eleves.ElevID 
       GROUP BY Activites.Theme,Activites.ActID,Eleves.Ville
       HAVING CONCAT(Activites.ActID,COUNT(Eleves.ElevID)) IN
-          (SELECT CONCAT(Activites.ActID,MAX(B.Nb_eleves)) -- table with the max of student by activity
-          FROM (SELECT Activites.ActID,COUNT(Eleves.ElevID) AS Nb_eleves -- table with the nb of student by activity and town
+          (SELECT CONCAT(Activites.ActID,MAX(B.Nb_eleves)) -- Table avec le max de participants d'une meme ville par activite
+          FROM (SELECT Activites.ActID,COUNT(Eleves.ElevID) AS Nb_eleves -- Table avec le nombre de participant d'une meme activite par ville
                FROM Repartition,Activites,Eleves WHERE Activites.ActID=Repartition.ActID AND Repartition.ElevID=Eleves.ElevID 
                GROUP BY Activites.ActID,Eleves.Ville) B,
           Activites WHERE Activites.ActID=B.ActID
@@ -106,7 +106,7 @@ GROUP BY Eleves.ElevID,Eleves.nom,Eleves.Ville;
 
 
 --  "------------------ QUESTION 15 ------------------"
-SELECT A.Ville,ROUND(A.Nb_eleves*100/SUM(A.Nb_eleves) OVER(),1) AS Pourcentage -- SUM(A.Nb_eleves) Over() Somme on all "Nb_eleves" on all row of "A"
+SELECT A.Ville,ROUND(A.Nb_eleves*100/SUM(A.Nb_eleves) OVER(),1) AS Pourcentage -- SUM(A.nb_student) Over() Somme sur tout "nb_student" (toutes les lignes)
 FROM (SELECT Eleves.Ville,COUNT(Eleves.ElevID) AS Nb_eleves    
      FROM Repartition,Activites,Eleves WHERE Activites.ActID=Repartition.ActID AND Repartition.ElevID=Eleves.ElevID 
      GROUP BY Eleves.Ville) A;
@@ -114,26 +114,26 @@ FROM (SELECT Eleves.Ville,COUNT(Eleves.ElevID) AS Nb_eleves
 
 --  "------------------ QUESTION 16 ------------------"
 DROP TABLE IF EXISTS Temp;
-CREATE TABLE Temp AS -- Create a table with just nom, id, "all" act_id, ClassID to simplify the rest
+CREATE TABLE Temp AS -- Creer une table avec nom, id, act_id, classe_id pour rendre la fin de la question plus facile
     SELECT Eleves.nom,Eleves.ElevID,Activites.ActID,Eleves.ClassID
     FROM Repartition,Activites,Eleves WHERE Activites.ActID=Repartition.ActID AND Repartition.ElevID=Eleves.ElevID;
  
-SELECT GROUP_CONCAT(DISTINCT(X.nom)) AS X,GROUP_CONCAT(DISTINCT(Y.nom)) AS Y -- Write GROUP_CONCAT(DISTINCT(Z.nom)) because Z.nom does not work. Student of different class different can have differents activity in commun so there are 2 or + row for them
+SELECT GROUP_CONCAT(DISTINCT(X.nom)) AS X,GROUP_CONCAT(DISTINCT(Y.nom)) AS Y -- On ecrit GROUP_CONCAT(DISTINCT(Z.nom)) car Z.nom ne suffit pas. Un couple d'etudiant de different classe peuvent avoir plus d'une activite en commun donc le couple apparait sur plusieurs lignes
 FROM Temp AS X,Temp AS Y -- duplicate the class crated
-WHERE X.ElevID<Y.ElevID AND X.ClassID!=Y.ClassID AND X.ActID=Y.ActID -- choise X.ElevID<Y.ElevID to don't have XY and YX in liste than choice just studants with same activite and not same class
+WHERE X.ElevID<Y.ElevID AND X.ClassID!=Y.ClassID AND X.ActID=Y.ActID -- On ecrit X.elevID<Y.elevID pour ne pas avoir XY et YX 
 GROUP BY X.ElevID,Y.ElevID; 
 
 DROP TABLE IF EXISTS Temp;
 
 
 --  "------------------ QUESTION 17 ------------------"
-SELECT Nb_per_couple.ActID, Nb_per_couple.ClassID
-FROM ( -- 2 subqueries to construct useful tables
+SELECT Nb_par_couple.ActID, Nb_par_couple.ClassID
+FROM ( -- on utilise 2 sous-requetes pour construire des tables et simplifier le probleme
     SELECT Activites.ActID, Eleves.ClassID, COUNT(Eleves.ElevID) AS Nb_eleves
     FROM Activites, Repartition, Eleves
     WHERE Activites.ActID=Repartition.ActID AND Repartition.ElevID=Eleves.ElevID
-    GROUP BY Activites.ActID, Eleves.ClassID) AS Nb_per_couple, ( -- first, create a table with the number of student per couple(activity,class)
+    GROUP BY Activites.ActID, Eleves.ClassID) AS Nb_par_couple, ( -- d'abord, on cree une table avec le nombre d'eleves par couple(activite,classe)
     SELECT Eleves.ClassID, COUNT(Eleves.ElevID) AS Nb_eleves
     FROM Eleves
-    GROUP BY Eleves.ClassID) AS Nb_per_class -- then, create a table with the number of student per class
-WHERE Nb_per_couple.ClassID=Nb_per_class.ClassID AND Nb_per_couple.Nb_eleves=Nb_per_class.Nb_eleves; -- finally, join on the ClassID and the couple must have the same number of students as the number of students of its class
+    GROUP BY Eleves.ClassID) AS Nb_par_classe -- ensuite, on cree une table avec le nombre d'eleves total par classe
+WHERE Nb_par_couple.ClassID=Nb_par_classe.ClassID AND Nb_par_couple.Nb_eleves=Nb_par_classe.Nb_eleves; -- enfin, on fait une jointure sur CLassID et on ne garde que les couples qui ont le meme nombre d'eleve que le nombre total d'eleve dans la classe du couple
